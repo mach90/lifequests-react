6e99e4ee86ce7e2268cdbf331ff28e5c9d705919
mach90 <rilem.benane@gmail.com>
2025-01-25 20:57:34 +0100

    before tailwind v4

--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -7,8 +7,10 @@ import { ImTree } from "react-icons/im";
 import { FaGear, FaHouseFlag, FaPerson } from "react-icons/fa6";
 
 const mainNavContainerStyle = "bg-container rounded-lg flex flex-row gap-4 justify-end items-center px-6 py-2 text-slate-200";
-const mainNavLinkStyle = "flex flex-col justify-center items-center hover:text-variant1 border-b-4 border-gray-800 hover:border-variant1 w-20";
-const mainNavLinkActiveStyle = "flex flex-col justify-center items-center text-variant1 border-b-4 border-variant1 w-20";
+const mainNavLinkStyle = "flex flex-col justify-center items-center hover:text-variant1 w-20";
+const mainNavLinkIconStyle = "text-2xl p-2 rounded-full border-4 border-gray-700 mb-1";
+const mainNavLinkTextStyle = "uppercase text-xs font-bold";
+const mainNavLinkActiveStyle = "flex flex-col justify-center items-center text-variant1 w-20";
 const mainNavSettingsStyle = "ml-4 hover:text-yellow-400";
 const mainNavSettingsActiveStyle = "ml-4 text-yellow-400";
 const mainNavLogoutStyle = "ml-4 hover:text-red-500";
@@ -22,13 +24,32 @@ function MainNav() {
 
     return (
         <nav className={mainNavContainerStyle}>
-            <NavLink to="character" className={({ isActive }) => isActive ? mainNavLinkActiveStyle : mainNavLinkStyle}><FaPerson /> Character</NavLink>
-            <NavLink to="skills" className={({ isActive }) => isActive ? mainNavLinkActiveStyle : mainNavLinkStyle}><ImTree /> Skills</NavLink>
-            <NavLink to="guilds" className={({ isActive }) => isActive ? mainNavLinkActiveStyle : mainNavLinkStyle}><FaHouseFlag /> Guilds</NavLink>
-            <NavLink to="quests" className={({ isActive }) => isActive ? mainNavLinkActiveStyle : mainNavLinkStyle}><FaScroll /> Quests</NavLink>
-            <NavLink to="inventory" className={({ isActive }) => isActive ? mainNavLinkActiveStyle : mainNavLinkStyle}><BsBackpack4Fill /> Inventory</NavLink>
-            <NavLink to="account" className={({ isActive }) => isActive ? mainNavSettingsActiveStyle : mainNavSettingsStyle}><FaGear size={24}/></NavLink>
-            <button onClick={handleLogout} className={mainNavLogoutStyle} disabled={isLoading}><MdLogout size={24}/></button>
+            <NavLink to="character" className={({ isActive }) => isActive ? mainNavLinkActiveStyle : mainNavLinkStyle}>
+                <div className={mainNavLinkIconStyle}><FaPerson /></div> 
+                <div className={mainNavLinkTextStyle}>Character</div> 
+            </NavLink>
+            <NavLink to="skills" className={({ isActive }) => isActive ? mainNavLinkActiveStyle : mainNavLinkStyle}>
+                <div className={mainNavLinkIconStyle}><ImTree /></div>
+                <div className={mainNavLinkTextStyle}>Skills</div>
+            </NavLink>
+            <NavLink to="guilds" className={({ isActive }) => isActive ? mainNavLinkActiveStyle : mainNavLinkStyle}>
+                <div className={mainNavLinkIconStyle}><FaHouseFlag /></div> 
+                <div className={mainNavLinkTextStyle}>Guilds</div>
+            </NavLink>
+            <NavLink to="quests" className={({ isActive }) => isActive ? mainNavLinkActiveStyle : mainNavLinkStyle}>
+                <div className={mainNavLinkIconStyle}><FaScroll /></div> 
+                <div className={mainNavLinkTextStyle}>Quests</div>
+            </NavLink>
+            <NavLink to="inventory" className={({ isActive }) => isActive ? mainNavLinkActiveStyle : mainNavLinkStyle}>
+                <div className={mainNavLinkIconStyle}><BsBackpack4Fill /></div> 
+                <div className={mainNavLinkTextStyle}>Inventory</div>
+            </NavLink>
+            <NavLink to="account" className={({ isActive }) => isActive ? mainNavSettingsActiveStyle : mainNavSettingsStyle}>
+                <FaGear size={24}/>
+            </NavLink>
+            <button onClick={handleLogout} className={mainNavLogoutStyle} disabled={isLoading}>
+                <MdLogout size={24}/>
+            </button>
         </nav>
     );
 };
